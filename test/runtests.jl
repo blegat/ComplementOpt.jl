e3969ed35fddecde748d9155bfdf9942004b0526
François Pacaud <frapac@users.noreply.github.com>
2025-12-04 17:18:46 +0100

    Add support for nonlinear expressions in MOI.Complements (#8)
    * add support for nonlinear expressions in MOI.Complements
    
    * operate!
    
    ---------
    
    Co-authored-by: Benoît Legat <benoit.legat@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,28 @@ function fletcher_leyffer_ex1_nonlinear_model()
     return model
 end
 
+function nonlinear_test_model()
+    model = Model()
+    @variable(model, x >= 0.0)
+    @variable(model, y >= 0.0)
+    @objective(model, Min, x^2 + y^2 - 4*x*y)
+    # Build complementarity constraints with nonlinear expression
+    @constraint(model, [sin(x), y] ∈ MOI.Complements(2))
+    return model
+end
+
+function nonlinear_test_reformulated_model()
+    model = Model()
+    @variable(model, x >= 0.0)
+    @variable(model, y >= 0.0)
+    @variable(model, slack)
+    @objective(model, Min, x^2 + y^2 - 4*x*y)
+    # Build complementarity constraints with nonlinear expression
+    @constraint(model, sin(x) == slack)
+    @constraint(model, slack * y <= 0.0)
+    return model
+end
+
 expected_models = Dict(
     Instances.fletcher_leyffer_ex1_model => fletcher_leyffer_ex1_nonlinear_model,
 )
@@ -41,6 +63,20 @@ function test_model(model_func)
     end
 end
 
+function test_nonlinear_expr()
+    model = nonlinear_test_model()
+    inner = MOI.Utilities.Model{Float64}()
+    set_optimizer(model, () -> ComplementOpt.Optimizer(inner))
+    MOI.Utilities.attach_optimizer(model)
+    MOI.Utilities.attach_optimizer(backend(model).optimizer.model)
+
+    expected = nonlinear_test_reformulated_model()
+    MOI.Bridges._test_structural_identical(
+        unsafe_backend(model).optimizer,
+        backend(expected),
+    )
+end
+
 instances = filter(names(Instances; all = true)) do name
     # The function types start with `#`
     s = String(name)
@@ -50,3 +86,9 @@ end
 @testset "$name" for name in instances
     test_model(getfield(Instances, name))
 end
+
+@testset "Unit-tests" begin
+    test_nonlinear_expr()
+end
+
+
