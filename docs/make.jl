aa8cc27f81b7ca2c652d090b03e56d7ec8dca8b4
fpacaud <francoispacaud8@gmail.com>
2026-04-29 22:12:36 +0200

    add github action for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -2,32 +2,28 @@ using Documenter
 using DocumenterInterLinks
 using MathOptComplements
 
-links = InterLinks(
-    "MathOptInterface" => "https://jump.dev/MathOptInterface.jl/stable/",
-)
+links = InterLinks("MathOptInterface" => "https://jump.dev/MathOptInterface.jl/stable/")
 
 makedocs(
     sitename = "MathOptComplements.jl",
     format = Documenter.HTML(
         assets = ["assets/favicon.ico"],
         prettyurls = Base.get(ENV, "CI", nothing) == "true",
-        mathengine = Documenter.KaTeX()
+        mathengine = Documenter.KaTeX(),
     ),
     modules = [MathOptComplements],
     repo = "https://github.com/blegat/MathOptComplements.jl/blob/{commit}{path}#{line}",
     checkdocs = :none,
-    clean=true,
+    clean = true,
     pages = [
         "Home" => "index.md",
         "Quickstart" => "quickstart.md",
-        "Tutorials" => [
-            "Equilibrium problem" => "equilibrium.md",
-        ],
+        "Tutorials" => ["Equilibrium problem" => "equilibrium.md"],
         "API reference" => [
             "Reformulations" => "api/reformulation.md",
             "Relaxations" => "api/relaxation.md",
             "Bridges" => "api/bridges.md",
-        ]
+        ],
     ],
     plugins = [links],
 )
