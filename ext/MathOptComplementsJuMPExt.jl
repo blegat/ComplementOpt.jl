9e16aa4b5a39ca8b031be213520c1a2f747c797b
Oscar Dowson <odow@users.noreply.github.com>
2026-05-20 14:20:18 +1200

    Add a LICENSE.md (#53)

--- ext/MathOptComplementsJuMPExt.jl
+++ ext/MathOptComplementsJuMPExt.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 François Pacaud, Benoît Legat, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module MathOptComplementsJuMPExt
 
 import MathOptComplements
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 François Pacaud, Benoît Legat, and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
 function _remove_bounds!(model::MOI.ModelLike, x::MOI.VariableIndex)
     for cidx in [
