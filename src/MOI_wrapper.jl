867ec899feafc6b69afd5f74e49762031358e8b4
Benoît Legat <benoit.legat@gmail.com>
2026-07-08 20:01:41 +0200

    Remove separate thing

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -24,7 +24,6 @@ end
 
 Optimizer(model::MOI.ModelLike) = Optimizer{Float64}(model)
 
-MOI.Bridges.supports_constraint_bridges(::Optimizer) = true
 MOI.Bridges.Constraint.bridges(model::Optimizer) = model.constraint_map
 
 """
